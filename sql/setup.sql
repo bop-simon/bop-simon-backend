6f7f84e676c3290a71ec9962afacb8e9fb159cfc
Julius Franklin <jjfranklin88@outlook.com>
2022-02-22 18:12:57 -0500

    adjusting sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,17 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS levels CASCADE;
+DROP TABLE IF EXISTS songs CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email TEXT NOT NULL,
+    username TEXT NOT NULL,
     password TEXT NOT NULL
 );
 
-CREATE TABLE levels (
+CREATE TABLE songs (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    easy VARCHAR NOT NULL,
-    medium VARCHAR NOT NULL,
-    hard VARCHAR NOT NULL
+    notes TEXT NOT NULL,
+    level INT NOT NULL
 );
+
+CREATE TABLES scores (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    song_id INT NOT NULL,
+    FOREIGN KEY (song_id) REFERENCES users(id),
+    score BIGINT NOT NULL
+)
